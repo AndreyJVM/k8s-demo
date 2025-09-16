d4ea21c2f176a6034087af8d83258f66bda3675f
Andrey V. <andrey.vorobev.aqa@gmail.com>
2025-09-16 07:56:47 +0300

    Update docker file, server.py

--- start/example/simple_app_1/server.py
+++ start/example/simple_app_1/server.py
@@ -1,13 +1,15 @@
+from reloading import reloading
 from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 from datetime import datetime
 
 class RequestHandler(BaseHTTPRequestHandler):
+    @reloading
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         now = datetime.now()
-        respons_string = now.strftime("The time is %-I:%M %p, UTC.")
+        respons_string = now.strftime("The time is %H:%M, UTC.")
         self.wfile.write(bytes(respons_string, "utf-8"))
 
 def startServer():
